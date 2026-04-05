efd172e37108ab932f417736b7e44c40e1bd96ed
Harsh28Pandey <harsh28.knp@gmail.com>
2026-04-05 17:21:44 +0530

    added interview report model and integrate the ai model (gemini) api key

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config()
 const app = require("../Backend/src/app.js")
 const connectDB = require("../Backend/src/config/database.js")
+// const invokeGeminiAi = require("./src/services/ai.service.js")
+// const { resume, selfDescription, jobDescription } = require("./src/services/temp.js")
+// const generateInterviewReport = require("./src/services/ai.service.js")
 
 //* database connection
 connectDB()
+
+//* for testing purpose only
+// invokeGeminiAi()
+// generateInterviewReport({ resume, selfDescription, jobDescription })
 
 const PORT = 3000
 
