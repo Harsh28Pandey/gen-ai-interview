bffaddacda9401da8c3b8e67aaef326bd9b3a008
Harsh28Pandey <harsh28.knp@gmail.com>
2026-04-12 10:44:27 +0530

    Update ai.service.js

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -87,7 +87,7 @@ Job Description: ${jobDescription}
 `;
 
         const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: "gemini-3-flash-preview",
             contents: prompt,
             config: {
                 responseMimeType: "application/json",
@@ -149,7 +149,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
